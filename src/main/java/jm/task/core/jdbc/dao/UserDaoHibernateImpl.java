d26b6028cf1c06466fdc2f8326cc13ba0fff87a3
EgorShabunin <egorshabunin1801@gmail.com>
2022-10-03 21:03:52 +0300

    Commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,11 +3,13 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private SessionFactory sessionFactory = Util.getInstance().getSessionFactory();
 
     public UserDaoHibernateImpl() {
 
@@ -16,13 +18,12 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
         Transaction transaction = null;
-       try(Session session = Util.getSessionFactory().openSession()) {
+       try(Session session = sessionFactory.openSession()) {
            transaction = session.beginTransaction();
            Query query = session.createSQLQuery("CREATE TABLE IF NOT EXISTS USER (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
                    "NAME VARCHAR(100), LASTNAME VARCHAR(100), AGE TINYINT(3))").addEntity(User.class);
            query.executeUpdate();
            session.getTransaction().commit();
-           session.close();
        } catch (Exception e) {
            if (transaction != null) {
                transaction.rollback();
@@ -34,7 +35,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
         Transaction transaction = null;
-        try(Session session = Util.getSessionFactory().openSession()) {
+        try(Session session = sessionFactory.openSession()) {
              transaction = session.beginTransaction();
              Query query = session.createSQLQuery("DROP TABLE IF EXISTS USER").addEntity(User.class);
              query.executeUpdate();
@@ -50,7 +51,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void saveUser(String name, String lastName, byte age) {
         Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.save(new User(name, lastName, age));
             transaction.commit();
@@ -65,7 +66,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void removeUserById(long id) {
         Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
            transaction = session.beginTransaction();
             User user = session.get(User.class,id);
             if (user != null) {
@@ -84,7 +85,7 @@ public class UserDaoHibernateImpl implements UserDao {
     public List<User> getAllUsers() {
         Transaction transaction = null;
         List<User> list = null;
-        try(Session session = Util.getSessionFactory().openSession()) {
+        try(Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             list = session.createQuery("from User", User.class).list();
             transaction.commit();
@@ -100,7 +101,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void cleanUsersTable() {
         Transaction transaction = null;
-        try(Session session = Util.getSessionFactory().openSession()) {
+        try(Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             Query query = session.createSQLQuery(" TRUNCATE TABLE USER").addEntity(User.class);
             query.executeUpdate();
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,28 +8,39 @@ import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
     private static final String HOST = "jdbc:mysql://localhost:3306/pp113";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
-    private static Connection connection = null;
+    private static Connection connection;
+    private static volatile Util instance;
     private static SessionFactory sessionFactory;
 
     public Util() {
         try {
-            if (connection == null || connection.isClosed()) {
-                connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
-                System.out.println("ok, connect");
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
+            connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
+            System.out.println("ok, connect");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public static Connection getConnection() {
         return connection;
+    }
+
+    public static Util getInstance() {
+        if (instance == null) {
+            synchronized (Util.class) {
+                if (instance == null) {
+                    instance = new Util();
+                }
+            }
+        }
+        return instance;
     }
 
     public static SessionFactory getSessionFactory() {

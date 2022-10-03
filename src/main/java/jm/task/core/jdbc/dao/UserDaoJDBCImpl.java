7c8fe6935ce0c071e163faeaa9a57a60c1f37a8d
EgorShabunin <egorshabunin1801@gmail.com>
2022-10-03 20:43:22 +0300

    Commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,19 +2,20 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private Util util = new Util();
 
     public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-        try (Statement statement = Util.getConnection().createStatement()) {
+        try (Statement statement = Util.getInstance().getConnection().createStatement()) {
             statement.execute("CREATE TABLE IF NOT EXISTS USER (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
                     "NAME VARCHAR(100), LASTNAME VARCHAR(100), AGE TINYINT(3))");
         } catch (SQLException e) {
@@ -23,7 +24,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        try (Statement statement = Util.getConnection().createStatement()) {
+        try (Statement statement = Util.getInstance().getConnection().createStatement()) {
             statement.execute("DROP TABLE IF EXISTS USER");
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -31,7 +32,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement("INSERT INTO USER (NAME, LASTNAME, AGE) VALUES (?, ?, ?)")) {
+        try (PreparedStatement preparedStatement = Util.getInstance().getConnection().prepareStatement("INSERT INTO USER (NAME, LASTNAME, AGE) VALUES (?, ?, ?)")) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
@@ -42,7 +43,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement("DELETE FROM USER WHERE ID=?")) {
+        try (PreparedStatement preparedStatement = Util.getInstance().getConnection().prepareStatement("DELETE FROM USER WHERE ID=?")) {
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -52,7 +53,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>();
-        try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement("SELECT * FROM USER");
+        try (PreparedStatement preparedStatement = Util.getInstance().getConnection().prepareStatement("SELECT * FROM USER");
              ResultSet resultSet = preparedStatement.executeQuery("SELECT * FROM USER");
         ) {
             while (resultSet.next()) {
@@ -69,7 +70,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (Statement statement = Util.getConnection().createStatement()) {
+        try (Statement statement = Util.getInstance().getConnection().createStatement()) {
             statement.execute(" TRUNCATE TABLE USER");
         } catch (SQLException e) {
             throw new RuntimeException(e);
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,26 +2,37 @@ package jm.task.core.jdbc.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
     private static final String HOST = "jdbc:mysql://localhost:3306/pp113";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
-    private static Connection connection = null;
+    private static Connection connection;
+    private static volatile Util instance;
 
     public Util() {
         try {
-            if (connection == null || connection.isClosed()) {
-                connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
-                System.out.println("ok, connect");
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
+            connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
+            System.out.println("ok, connect");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public static Connection getConnection() {
         return connection;
+    }
+
+    public static Util getInstance() {
+        if (instance == null) {
+            synchronized (Util.class) {
+                if (instance == null) {
+                    instance = new Util();
+                }
+            }
+        }
+        return instance;
     }
 }
 
